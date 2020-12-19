07d7736253193dd138a845eba995ca20a9379857
andi <andi@mail>
2020-12-19 16:29:07 +0100

    Added Maven support, fixed stuff

---
+++ src/test/java/org/andiiiiiiiiii/pdfextractor/test/ExtractorTests.java
@@ -0,0 +1,34 @@
+package org.andiiiiiiiiii.pdfextractor.test;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExtractorTests {
+
+    @BeforeEach
+    void beforeEach() {
+
+    }
+
+    @AfterEach
+    void afterEach() {
+
+    }
+
+    @BeforeAll
+    static void init() {
+
+    }
+
+    @AfterAll
+    static void cleanUp() {
+
+    }
+
+
+    @Test
+    void helloWorldTest() {
+        System.out.println("Test executed");
+        assertTrue(true);
+    }
+}
